587af276289a7d9d8058d36e4d67c0ba1ebe7c48
ItaloCoppolella <97603806+ItaloCoppolella@users.noreply.github.com>
2024-04-26 12:15:37 +0200

    Fix Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args)  throws InterruptedException{
 
-		MoraCinese moracinese = new MoraCinese();
+		MorraCinese moracinese = new MorraCinese();
 
 		Scanner tastiera = new Scanner(System.in);
 
